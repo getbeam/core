fd9fb276e1a745c60ab89011b4d49c927c33e480
Timo Mämecke <maemecketimo@gmail.com>
2016-05-08 04:59:57 +0200

    indent res.json with 2 spaces

--- app/src/subapp-v1.js
+++ app/src/subapp-v1.js
@@ -10,6 +10,9 @@ const ErrorHandler = require("./utils/error-handler");
 
 const v1 = express();
 
+// Indent res.json with 2 spaces
+v1.set("json spaces", 2);
+
 v1.use(methodOverride());
 v1.use(bodyParser.json());
 v1.use(expressValidator());
